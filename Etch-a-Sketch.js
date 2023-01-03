fc27e15b55bdb549d882c86d5ef48e98e7443e8e
dnevin234 <dnevin234@gmail.com>
2023-01-03 15:14:27 +1300

    Added button interface with Start and Reset functionality

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -1,19 +1,30 @@
-/* Gather Row and Column count from user */
-var gridNum = prompt("How many Rows and Columns do you want?");
-
-/* Create base container div */
-var grid = document.createElement('div');
-grid.className='grid';
-
-/* Loop through the horizontal axis and create X number of columns with X number of children on the vertical axis */
-for (var i=0; i < gridNum; i++) {
-    var column = document.createElement('div');
-    column.className = 'column';
-    for (var j=0 ; j < gridNum ; j++) {
-        var row = document.createElement('div');
-        row.className = 'row';
-        column.appendChild(row);
-    }
-    grid.appendChild(column);
+function resetDiv() {
+    /* Remove the existing grid div and recall the grid generation function */
+    document.getElementById("grid-container").remove();
+    generateGrid();
 }
-document.body.appendChild(grid);
+
+function generateGrid() {
+    /* Gather Row and Column count from user */
+    
+    var gridNum = prompt("How many Rows and Columns do you want?");
+
+    /* Create base container div */
+    var grid = document.createElement('div');
+    grid.setAttribute("id", "grid-container")
+    grid.className = 'grid';
+
+    /* Loop through the horizontal axis and create X number of columns with X number of rows */
+    for (var i = 0; i < gridNum; i++) {
+        var column = document.createElement('div');
+        column.className = 'column';
+        for (var j = 0 ; j < gridNum ; j++) {
+            var row = document.createElement('div');
+            row.className = 'row';
+            column.appendChild(row);
+        }
+        grid.appendChild(column);
+    }
+    /* Append formed grid to document body */
+    document.body.appendChild(grid);
+}
